223d9fd6d5238262612db751621a73703fa73872
Alessandro Furlanetto <alexfurla@hotmail.com>
2022-04-02 23:14:01 +0200

    Add project files.

---
+++ TestKafkaRider/MessageConsumer.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestKafkaRider
+{
+    public class MessageConsumer : IConsumer<Message>
+    {
+        public Task Consume(ConsumeContext<Message> context)
+        {
+            Console.Write(context.Message.Text);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TestKafkaRider/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+// https://masstransit-project.com/usage/riders/kafka.html
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using TestKafkaRider;
+
+namespace KafkaTest
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var services = new ServiceCollection();
+
+            services.AddMassTransit(x =>
+            {
+                // x.UsingRabbitMq((context, cfg) => cfg.ConfigureEndpoints(context));
+
+                x.AddRider(rider =>
+                {
+                    rider.AddProducer<Message>("topic-name");
+
+                    rider.UsingKafka((context, k) =>
+                    {
+                        k.Host("localhost:9092");
+                    });
+                });
+            });
+
+            var provider = services.BuildServiceProvider();
+
+            var busControl = provider.GetRequiredService<IBusControl>();
+
+            await busControl.StartAsync(new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token);
+            try
+            {
+                var producer = provider.GetRequiredService<ITopicProducer<Message>>();
+                do
+                {
+                    string value = await Task.Run(() =>
+                    {
+                        Console.WriteLine("Enter text (or quit to exit)");
+                        Console.Write("> ");
+                        return Console.ReadLine();
+                    });
+
+                    if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+                        break;
+
+                    await producer.Produce(new
+                    {
+                        Text = value
+                    });
+                }
+                while (true);
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
